4aaa2f83682ad385cb6b64aa3b5c75e314903ba4
Leonardo-Cardoso-ie <leonardocardoso.ie@gmail.com>
2023-01-12 23:48:12 +0000

    Change strings from variables to Int to be interpreted as numbers

--- run.py
+++ run.py
@@ -3,4 +3,18 @@ import random
 name = input('Enter your name: ')
 print('Welcome to play this game', name,'good luck!')
 
+# Minimum number
+min_number = int(input('Please enter minimum number: '))
+# Maximum number
+max_number = int(input('Please enter maximum number: '))
+# randint() generates random integers between min and max numbers.
+rand_number = random.randint(min_number, max_number)
+# Number of chances
 
+# Tell us if we guessed too high or low
+
+# Tell us if we are right or wrong
+
+# Prompt if win or lose
+
+# Keep score and give option to retry
