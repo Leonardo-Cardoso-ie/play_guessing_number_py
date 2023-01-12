083124da52ede2a6efcc7155eed0d8d203aa3e00
Leonardo-Cardoso-ie <leonardocardoso.ie@gmail.com>
2023-01-12 23:21:58 +0000

    First commit

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+name = input('Enter your name: ')
+print('Welcome to play this game', name,'good luck!')
+
+
